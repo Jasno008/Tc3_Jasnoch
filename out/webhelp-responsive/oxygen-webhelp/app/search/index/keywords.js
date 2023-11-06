101d767580412286226e140c3633522c9e9aaa5d
Jasno008 <Jasno008@umn.edu>
2023-11-05 21:15:55 -0600

    1

--- out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -1,9 +1,15 @@
-var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Accessibility",p:["p1"]},{w:"Options",p:["p1"]},{w:"***",p:["p2"]},{w:"What",p:["p3"]},{w:"is",p:["p3"]},{w:"Multi-Factor",p:["p3"]},{w:"Authentication?",p:["p3"]}];
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Accessibility",p:["p1"]},{w:"Options",p:["p1"]},{w:"Managing",p:["p2"]},{w:"Your",p:["p2"]},{w:"Duo",p:["p2","p5","p7","p9"]},{w:"Devices",p:["p2"]},{w:"***",p:["p3"]},{w:"What",p:["p4"]},{w:"is",p:["p4"]},{w:"Multi-Factor",p:["p4"]},{w:"Authentication?",p:["p4"]},{w:"Adding",p:["p5"]},{w:"a",p:["p5","p9"]},{w:"Device",p:["p5","p6","p8","p9"]},{w:"to",p:["p5","p7"]},{w:"Compatibility",p:["p6"]},{w:"How",p:["p7"]},{w:"Enable",p:["p7"]},{w:"Authentication",p:["p7"]},{w:"Reactivate",p:["p8"]},{w:"an",p:["p8"]},{w:"Existing",p:["p8"]},{w:"Removing",p:["p9"]},{w:"From",p:["p9"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
 ph["p1"]=[3, 4];
-ph["p2"]=[5];
-ph["p3"]=[6, 7, 8, 9];
+ph["p2"]=[5, 6, 7, 8];
+ph["p3"]=[9];
+ph["p4"]=[10, 11, 12, 13];
+ph["p5"]=[14, 15, 16, 17, 7];
+ph["p6"]=[16, 18];
+ph["p7"]=[19, 17, 20, 7, 21];
+ph["p8"]=[22, 23, 24, 16];
+ph["p9"]=[25, 15, 16, 26, 7];
 var keywordsInfo = {
     keywords: keywords,
     ph: ph
--- out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,2 +1,2 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-var linkToParent = {4:-1,1:4,2:-1,3:-1};
+var linkToParent = {5:-1,8:5,7:2,9:2,2:-1,10:2,6:2,1:5};
